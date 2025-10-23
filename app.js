3feb1ffa71c828ebac5524e35a84de8d655d17b0
YooInHak <dlsgkr2856@gmail.com>
2025-10-23 11:32:11 +0900

    [ADD] user,product에 필터링, 정렬 기능 추가

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ const prisma = new PrismaClient();
 
 //users routes
 app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
+  const { offset = 0, limit = 0, order = "newest" } = req.query;
+  let orderBy;
+  switch (order) {
+    case "oldest":
+      orderBy = { createdAt: "asc" };
+      break;
+    case "newest":
+      orderBy = { createdAt: "desc" };
+      break;
+    default:
+      orderBy = { createdAt: "desc" };
+  }
+  const users = await prisma.user.findMany({
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit) || undefined,
+  });
   console.log(users);
   res.send(users);
 });
@@ -56,7 +72,31 @@ app.listen(port, () => {
 //product routes
 
 app.get("/products", async (req, res) => {
-  const product = await prisma.product.findMany();
+  const { offset = 0, limit = 0, order = "newest", category } = req.query;
+  let orderBy;
+  switch (order) {
+    case "oldest":
+      orderBy = { createdAt: "asc" };
+      break;
+    case "newest":
+      orderBy = { createdAt: "desc" };
+      break;
+    case "priceLowest":
+      orderBy = { price: "asc" };
+      break;
+    case "priceHighest":
+      orderBy = { price: "desc" };
+      break;
+    default:
+      orderBy = { createdAt: "desc" };
+  }
+  const where = category ? { category } : {};
+  const product = await prisma.product.findMany({
+    where,
+    orderBy,
+    skip: parseInt(offset),
+    take: parseInt(limit) || undefined,
+  });
   console.log(product);
   res.send(product);
 });
